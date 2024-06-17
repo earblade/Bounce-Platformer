aa00c83481910e9df9f17818e70228a9668ca74d
earblade <tjlknuckles@hotmail.com>
2024-06-17 20:25:53 +0800

    finishing touches & bug fix

--- scenes/globals/global_variables.gd
+++ scenes/globals/global_variables.gd
@@ -8,3 +8,5 @@ var width: int = ProjectSettings.get_setting("display/window/size/viewport_width
 var player_height: int #pretty sure this is unused (set in player but not used anywhere)
 
 var game_over_flag: bool = false
+
+var is_window_set_correctly: bool = false
